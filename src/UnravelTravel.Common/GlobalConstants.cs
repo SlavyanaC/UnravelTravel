d3f220281d999e7d43962df20ca6d68857983fcb
Slavyana <slaviana95@gmail.com>
2018-12-05 23:08:10 +0200

    Automapper

---
+++ src/UnravelTravel.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace UnravelTravel.Common
+{
+    public static class GlobalConstants
+    {
+        public const string AdministratorRoleName = "Administrator";
+
+        public const string UserRoleName = "User";
+    }
+}
---
+++ src/UnravelTravel.Common/Mapping/IMapFrom.cs
@@ -0,0 +1,7 @@
+﻿namespace UnravelTravel.Common.Mapping
+{
+    // ReSharper disable once UnusedTypeParameter
+    public interface IMapFrom<T>
+    {
+    }
+}
