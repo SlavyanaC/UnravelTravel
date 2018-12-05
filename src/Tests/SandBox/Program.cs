51c045d1f2221f27526523ce845b8596546987dc
Slavyana <slaviana95@gmail.com>
2018-12-05 23:07:58 +0200

    Sandbox

---
+++ src/Tests/SandBox/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Sandbox
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Newtonsoft.Json;
+    using UnravelTravel.Data.Models;
+    using UnravelTravel.Data;
+    using UnravelTravel.Data.Common.Repositories;
+    using UnravelTravel.Data.Repositories;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"{typeof(Program).Namespace} ({string.Join(" ", args)}) starts working...");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(true);
+
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                serviceProvider = serviceScope.ServiceProvider;
+                SandboxCode(serviceProvider);
+            }
+        }
+
+        private static void SandboxCode(IServiceProvider serviceProvider)
+        {
+            //var context = serviceProvider.GetService<ApplicationDbContext>();
+            //var jsonString = File.ReadAllText("countries.json");
+            //var countries = JsonConvert.DeserializeObject<Country[]>(jsonString);
+            //context.Countries.AddRange(countries);
+            //context.SaveChanges();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+        }
+    }
+}
