78581c82f1a389c0b3b5e1b642bc68c721a02577
Slavyana <slaviana95@gmail.com>
2018-12-18 14:00:23 +0200

    TopActivities and TopRestaurants are now ordered by avg rating

--- src/Services/UnravelTravel.Services.Data/Models/Destinations/DestinationDetailsViewModel.cs
+++ src/Services/UnravelTravel.Services.Data/Models/Destinations/DestinationDetailsViewModel.cs
@@ -1,12 +1,10 @@
-﻿using AutoMapper;
-using UnravelTravel.Services.Data.Models.Activities;
-
-namespace UnravelTravel.Services.Data.Models.Destinations
+﻿namespace UnravelTravel.Services.Data.Models.Destinations
 {
     using System.Collections.Generic;
     using System.Linq;
 
     using UnravelTravel.Data.Models;
+    using UnravelTravel.Services.Data.Models.Activities;
     using UnravelTravel.Services.Data.Models.Restaurants;
     using UnravelTravel.Services.Mapping;
 
@@ -20,14 +18,20 @@ namespace UnravelTravel.Services.Data.Models.Destinations
 
         public string Information { get; set; }
 
-        // TODO: Use IHaveCustomMapping and delete Locations prop
         public ICollection<Location> Locations { get; set; }
 
-        public ICollection<ActivityViewModel> Activities => this.Locations.SelectMany(l => l.Activities).AsQueryable().To<ActivityViewModel>().ToList();
+        public ICollection<ActivityViewModel> Activities =>
+            this.Locations.SelectMany(l => l.Activities).AsQueryable().To<ActivityViewModel>().ToList();
+
+        public ICollection<ActivityViewModel> TopActivities => 
+            this.Activities.OrderByDescending(a => a.AverageRating).Take(3).ToList();
 
         public int TotalActivities => this.Activities.Count();
 
         public ICollection<RestaurantViewModel> Restaurants { get; set; }
+
+        public ICollection<RestaurantViewModel> TopRestaurants =>
+            this.Restaurants.OrderByDescending(r => r.AverageRating).Take(3).ToList();
 
         public int TotalRestaurants => this.Restaurants.Count();
 
