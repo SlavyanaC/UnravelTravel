ad0dd6a81682eecabf185ca7b6e13417f4cb25a9
Slavyana <slaviana95@gmail.com>
2019-01-03 18:11:38 +0200

    Added Activity info to Db

--- src/Data/UnravelTravel.Data.Models/Activity.cs
+++ src/Data/UnravelTravel.Data.Models/Activity.cs
@@ -24,6 +24,10 @@
 
         public decimal Price { get; set; }
 
+        public string Description { get; set; }
+
+        public string AdditionalInfo { get; set; }
+
         public int LocationId { get; set; }
 
         public virtual Location Location { get; set; }
